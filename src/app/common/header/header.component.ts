51e3bb6c469107ee3a4c9be79fc3175f04e18a29
sesh10 <sesha1011@gmail.com>
2020-06-01 21:38:49 +0530

    Initial files, Navbar and scss files

---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: "first-header",
+    templateUrl: "./header.component.html",
+    styleUrls: ["./header.component.scss"]
+})
+
+export class HeaderComponent {
+
+}
