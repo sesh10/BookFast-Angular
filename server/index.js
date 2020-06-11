5fd3b920e7111835b3d35f505c42d2f48a5cdf86
sesh10 <sesha1011@gmail.com>
2020-06-11 21:20:20 +0530

    Added basic server setup, added rental routes, added mongoDB connection

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/dev');
+const FakeDb = require('./fake-db');
+const rentalRoutes = require('./routes/rentals');
+
+mongoose.connect(config.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    const fakeDb = new FakeDb();
+    fakeDb.seedDb();
+});
+
+const app = express();
+
+app.use('/api/v1/rentals', rentalRoutes);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, function () {
+    console.log("I am runnin"); 
+});
